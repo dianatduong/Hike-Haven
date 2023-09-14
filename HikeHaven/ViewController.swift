18132deb711336ce8156312c32c4c43e238136a6
“Diana <dduong013@gmail.com>
2023-09-13 21:47:22 -0500

    delete storyboards

---
+++ HikeHaven/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HikeHaven
+//
+//  Created by Diana Duong on 9/13/23.
+//  Copyright © 2023 Diana Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
