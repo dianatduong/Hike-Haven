018276af8ac331712e91c2ba7da69a4152a2d5a0
“Diana <dduong013@gmail.com>
2023-09-13 21:57:04 -0500

    setup header

--- HikeHaven/ViewController.swift
+++ HikeHaven/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    lazy var headerTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Popular Trails"
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let headerView = UIView()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpHeader()
+        
+    }
+
+ 
+    
+    func setUpHeader() {
+        headerView.addSubview(headerTitle)
+        tableView.tableHeaderView = headerView
+        tableView.tableHeaderView?.frame.size.height = 60
+        NSLayoutConstraint.activate([
+            headerTitle.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 12),
+            headerTitle.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20)
+        ])
     }
 
 
