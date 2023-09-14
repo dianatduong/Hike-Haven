38ecda21927f2253bc5c8f124a2b85af90a64e3b
“Diana <dduong013@gmail.com>
2023-09-13 22:11:29 -0500

    add images struct

---
+++ HikeHaven/Images.swift
@@ -0,0 +1,24 @@
+//
+//  Images.swift
+//  HikeHaven
+//
+//  Created by Diana Duong on 9/13/23.
+//  Copyright © 2023 Diana Duong. All rights reserved.
+//
+
+import UIKit
+
+    struct UnSplashStruct: Codable {
+        let results: [UnSplashData]
+    }
+
+
+    struct UnSplashData: Codable {
+        //let description: String?
+        let urls: ImageURLS
+        
+    }
+
+    struct ImageURLS: Codable {
+        let thumb: String?
+    }
